ec630ba968e58cfb6be4495ad1706124fb878c68
Dan Taylor <yoblin@gmail.com>
2024-12-01 21:16:23 -0800

    update preview.py to support multiple objects

--- preview.py
+++ preview.py
@@ -15,32 +15,33 @@ def main():
     width = img.size[0]
     height = img.size[1]
 
-    x = []
-    y = []
-
     # YOLOv11 labels file format:
     # https://docs.ultralytics.com/datasets/segment/#supported-dataset-formats
     with open(args.labels) as polygons:
         for line in polygons:
+            x = []
+            y = []
+
             points = line.split()[1:]
             x = points[::2] # all even indexes
             y = points[1::2] # all odd indexes
-            break # we are only supporting a single label right now
 
-    # Convert from percentages to actual pixel values
-    x = [float(pt) * width for pt in x]
-    y = [float(pt) * height for pt in y]
+            # Convert from percentages to actual pixel values
+            x = [float(pt) * width for pt in x]
+            y = [float(pt) * height for pt in y]
 
-    # convert values to ints
-    x = map(int, x)
-    y = map(int, y)
+            # convert values to ints
+            x = map(int, x)
+            y = map(int, y)
 
-    img2 = img.copy()
-    draw = ImageDraw.Draw(img2)
-    draw.polygon(list(zip(x,y)), fill = "wheat")
+            img2 = img.copy()
+            draw = ImageDraw.Draw(img2)
+            draw.polygon(list(zip(x,y)), fill = "green")
 
-    img3 = Image.blend(img, img2, 0.5)
-    img3.save('preview.png')
+            img3 = Image.blend(img, img2, 0.5)
+            img = img3
+
+        img.save('preview.png')
 
 if __name__ == "__main__":
     main()
